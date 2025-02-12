8c256f190585fcf109136933c631c574e878ca97
RahardiSalim <rahardi.salim@ui.ac.id>
2025-02-12 07:43:54 +0700

    Implement Edit Product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import java.util.UUID;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +12,33 @@ import java.util.List;
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
+    // Create product
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
+    }
+
+    // Find product by ID
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    // Update an existing product
+    public Product update(Product product) {
+        for (int i = 0; i < productData.size(); i++) {
+            Product p = productData.get(i);
+            if (p.getProductId().equals(product.getProductId())) {
+                productData.set(i, product);
+                return product;
+            }
+        }
+        return null;
     }
 
     public Iterator<Product> findAll() {
