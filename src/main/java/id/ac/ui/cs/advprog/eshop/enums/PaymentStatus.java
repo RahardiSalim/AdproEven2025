fe1d9b4af3457c79d219e13850fd8ddb3277ff04
RahardiSalim <rahardi.salim@ui.ac.id>
2025-03-07 18:37:42 +0700

    [GREEN] Implement PaymentMethod and PaymentStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -1,5 +1,17 @@
 package id.ac.ui.cs.advprog.eshop.enums;
 
 public enum PaymentStatus {
+    WAITING("WAITING"),
+    SUCCESS("SUCCESS"),
+    REJECTED("REJECTED");
 
+    private final String value;
+
+    PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
